f21a4e796a0217ac0e6dc50db6c62be98e6edd72
Bektur777 <ubei003@mail.ru>
2022-12-20 00:30:06 +0600

    Adding method to view position salary and user payroll

--- src/main/java/org/example/controllers/AccountantController.java
+++ src/main/java/org/example/controllers/AccountantController.java
@@ -3,9 +3,12 @@ package org.example.controllers;
 import org.example.dao.AccountantDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/accountant")
 public class AccountantController {
     private AccountantDAO accountantDAO;
 
@@ -14,6 +17,16 @@ public class AccountantController {
         this.accountantDAO = accountantDAO;
     }
 
+    @GetMapping("/position")
+    public String positionSalary(Model model) {
+        model.addAttribute("salaries", accountantDAO.getPositionSalary());
+        return "accountant/position_salary";
+    }
 
+    @GetMapping("/payroll")
+    public String userPayroll(Model model) {
+        model.addAttribute("payrolls", accountantDAO.getUserPayroll());
+        return "accountant/user_payroll";
+    }
 
 }
--- src/main/java/org/example/dao/AccountantDAO.java
+++ src/main/java/org/example/dao/AccountantDAO.java
@@ -1,7 +1,29 @@
 package org.example.dao;
 
+import org.example.model.Payroll;
+import org.example.model.Position;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class AccountantDAO {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public AccountantDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Position> getPositionSalary() {
+        return jdbcTemplate.query("SELECT * FROM position", new BeanPropertyRowMapper<>(Position.class));
+    }
+
+    public List<Payroll> getUserPayroll() {
+        return jdbcTemplate.query("SELECT * FROM payroll", new BeanPropertyRowMapper<>(Payroll.class));
+    }
+
 }
