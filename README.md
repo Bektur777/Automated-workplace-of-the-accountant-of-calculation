46f37752e7fb0db0c3609ce25fcfcb3fee2c1834
Bektur777 <ubei003@mail.ru>
2022-12-20 00:18:10 +0600

    Adding README.md file

---
+++ README.md
@@ -0,0 +1,2 @@
+# Automated-workplace-of-the-accountant-of-calculation
+<a href="doc/README.md">Technical Requirements</a>
--- src/main/java/org/example/controllers/AccountantController.java
+++ src/main/java/org/example/controllers/AccountantController.java
@@ -1,5 +1,19 @@
 package org.example.controllers;
 
+import org.example.dao.AccountantDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AccountantController {
+    private AccountantDAO accountantDAO;
+
+    @Autowired
+    public AccountantController(AccountantDAO accountantDAO) {
+        this.accountantDAO = accountantDAO;
+    }
+
+
 
 }
